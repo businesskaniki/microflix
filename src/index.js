720d583b9446692482af9a1cd836a1d33ddc65ec
unknown <aquinasmoriasi@gmail.com>
2022-08-04 15:24:31 +0300

    minor styling changes on like button

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import './style.css';
 import './desktop.css';
 import { displayMovies, displayComments } from './modules/fetch.js';
 
-window.addEventListener('DOMContentLoaded', () => {
-  displayMovies();
-  displayComments();
-});
+displayMovies();
+displayComments();
