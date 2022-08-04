88699d7ab4eeb4e2458c25561555abb50a296fe4
businesskaniki <businesskanikii@gmail.com>
2022-08-04 22:48:28 +0300

    adding likes functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import './style.css';
 import './desktop.css';
-import fetchdata from './modules/fetch.js';
-import likes from './modules/likes.js';
+import { fetchdata } from './modules/fetch.js';
+import { likesdata } from './modules/likes.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   fetchdata();
-  likes();
+  likesdata();
 });
-
--- src/modules/fetch.js
+++ src/modules/fetch.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable max-len */
 /* eslint-disable consistent-return */
 const popupDetails = async (id) => {
   const data = await fetch(`https://api.tvmaze.com/shows/${id}`);
@@ -15,7 +17,7 @@ const popupDetails = async (id) => {
     <ul class="comments">
         <li>12:78 Best movie I ever watched</li>
     </ul>
-   <form action="#">
+   <form  action="#">
     <input type="text" placeholder="Your name">
     <textarea name="comments"  class = "add-comment" placeholder="Comment"></textarea>
     <button type="submit" id = "submit">Comment</button>
@@ -42,6 +44,7 @@ const popupDetails = async (id) => {
 
 const div = document.querySelector('.cards');
 const fetchdata = async () => {
+  fetchLikes();
   const data = await fetch('https://api.tvmaze.com/shows');
   try {
     const response = await data.json();
@@ -51,15 +54,40 @@ const fetchdata = async () => {
       const movie = response[movies];
       card.id = `${movie.id}`;
       card.innerHTML += `
-                <p><span>${movie.name}</span><i class="bi bi-heart-fill"></i><i class = "likes">5 likes</i></p>
+                <p><span>${movie.name}</span><i class="bi bi-heart-fill"></i><i class = "likes" id= "like${movie.id}"></i></p>
                 <button class= "open-comments" >comments</button>
       `;
-      card.style.backgroundImage = `url(${movie.image.medium})`;
+      card.style.backgroundImage = `url(${movie.image.original})`;
       div.append(card);
     }
   } catch (error) {
     return error;
   }
+
+  const likebtn = document.querySelectorAll('.bi-heart-fill');
+  likebtn.forEach((btn) => {
+    const likeid = (btn.parentNode.parentNode.getAttribute('id'));
+    btn.addEventListener('click', () => {
+      liking();
+    });
+    const liking = async () => {
+      const data = await fetch(
+        'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/9YE3WJxp5XfKqI5kUFRZ/likes',
+        {
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+          method: 'POST',
+          body: JSON.stringify({
+            id: likeid,
+          }),
+        },
+      );
+      const response = await data.json();
+      return response;
+    };
+  });
+
   const comments = document.querySelectorAll('.open-comments');
   comments.forEach((comment) => {
     comment.addEventListener('click', (e) => {
@@ -68,6 +96,17 @@ const fetchdata = async () => {
       popupDetails(e.target.parentNode.id);
       window.scroll({ top: 0, left: 0 });
     });
+  });
+};
+
+const fetchLikes = async () => {
+  const likesData = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/9YE3WJxp5XfKqI5kUFRZ/likes');
+  const response = await likesData.json();
+  response.forEach((likes) => {
+    if (typeof (likes.item_id) === 'string') {
+      const liketext = document.querySelector(`#like${likes.item_id}`);
+      liketext.innerHTML = `${likes.likes} likes(s)`;
+    }
   });
 };
 
